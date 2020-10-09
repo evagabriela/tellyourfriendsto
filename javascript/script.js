c0017d575b3d06cf10d7b0e2b2973ff2de9f3321
Keith Tom <keith.tom@gmail.com>
2020-10-09 12:48:03 -0700

    Fix randomly showing an action
    - rename tell to action-card

--- javascript/script.js
+++ javascript/script.js
@@ -1,39 +1,44 @@
 // main js file
-function showRandomTell() {
-  // make sure we reset all tells to hidden
-  $(".tell").removeClass("selected");
+function showRandomAction() {
+  // make sure we reset all action cards to hidden
+  $(".action-card").removeClass("selected");
 
   // optionally scope to specific hashtag if anchor present
-  var hashtag = $(location).attr('hash');
-  var selector = ".tell" + hashtag.replace("#", ".");
+  var hashtag = $(location).attr("hash");
+  var selector = ".action-card" + hashtag.replace("#", ".");
 
-  // pick a tell at random, where max is the number of tells (within the scope)
+  // pick a action card at random, where max is the number of action cards (within the scope)
   var max = $(selector).length;
-  var random = Math.floor(Math.random()*max);
+  var random = Math.floor(Math.random() * max);
 
-  $(selector).eq(random).addClass("selected");
+  $(selector)
+    .eq(random)
+    .addClass("selected");
 }
 
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+function getUrlVars() {
+  var vars = [],
+    hash;
+  var hashes = window.location.href
+    .slice(window.location.href.indexOf("?") + 1)
+    .split("&");
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split("=");
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
 }
 
-$(document).ready(function(){
-  showRandomTell();
+$(document).ready(function() {
+  showRandomAction();
 
-  $('.hashtag a').click(function(){ showRandomTell(); });
+  $(".hashtag a").click(function() {
+    showRandomAction();
+  });
 
   // helper for debugging production
   if (getUrlVars()["show"]) {
-    $(".tell").addClass("selected");
+    $(".action-card").addClass("selected");
   }
 });
